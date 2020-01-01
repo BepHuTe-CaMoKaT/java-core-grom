e26abe165fc92d86dc44b4c08de57090fbc71dc2
BePHuTe_CaMoKaT <eban7346@gmail.com>
2020-01-01 11:45:42 +0400

    third

--- src/lesson20/task2/Controller.java
+++ src/lesson20/task2/Controller.java
@@ -1,7 +1,8 @@
 package lesson20.task2;
 
 public class Controller {
-    public Transaction save(Transaction transaction){return null;}
+    private TransactionDAO transactionDAO = new TransactionDAO();
+    public Transaction save(Transaction transaction) throws Exception{return transactionDAO.save(transaction);}
     public Transaction[] transactionList(){return null;}
     public Transaction[] transactionList(String city){return null;}
     public Transaction[] transactionList(int amount){return null;}
--- src/lesson20/task2/TransactionDAO.java
+++ src/lesson20/task2/TransactionDAO.java
@@ -1,19 +1,32 @@
 package lesson20.task2;
 
+import lesson20.task2.exception.LimitExceeded;
+
 import java.util.Calendar;
 import java.util.Date;
 
 public class TransactionDAO {
-    private Transaction[] transactions=new Transaction[10];
+    private Transaction[] transactions = new Transaction[10];
+    private Utils utils = new Utils();
 
-    public Transaction save(Transaction transaction){
+    public Transaction save(Transaction transaction) throws Exception {
+        validate(transaction);
+        return transaction;
+    }
+
+    public Transaction[] transactionList() {
         return null;
     }
-    public Transaction[] transactionList(){return null;}
-    public Transaction[] transactionList(String city){return null;}
-    public Transaction[] transactionList(int amount){return null;}
 
-    private Transaction[]getTransactionsPerDay(Date dateOfCurTransaction){
+    public Transaction[] transactionList(String city) {
+        return null;
+    }
+
+    public Transaction[] transactionList(int amount) {
+        return null;
+    }
+
+    private Transaction[] getTransactionsPerDay(Date dateOfCurTransaction) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(dateOfCurTransaction);
 
@@ -21,28 +34,49 @@ public class TransactionDAO {
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         int count = 0;
-        for (Transaction transaction:transactions){
-            if (transaction!=null){
+        for (Transaction transaction : transactions) {
+            if (transaction != null) {
                 calendar.setTime(transaction.getDateCreated());
                 int trmonth = calendar.get(Calendar.MONTH);
                 int trday = calendar.get(Calendar.DAY_OF_MONTH);
-                if (trmonth==month&&trday==day) count++;
+                if (trmonth == month && trday == day) count++;
             }
         }
 
         Transaction[] result = new Transaction[count];
         int index = 0;
-        for (Transaction transaction:transactions){
-            if (transaction!=null){
+        for (Transaction transaction : transactions) {
+            if (transaction != null) {
                 calendar.setTime(transaction.getDateCreated());
                 int trmonth = calendar.get(Calendar.MONTH);
                 int trday = calendar.get(Calendar.DAY_OF_MONTH);
-                if (trmonth==month&&trday==day) {
-                    result[index]=transaction;
+                if (trmonth == month && trday == day) {
+                    result[index] = transaction;
                     index++;
                 }
             }
         }
         return result;
+    }
+
+    private void validate(Transaction transaction) throws Exception {
+//        - сумма транзакции больше указанного лимита
+//        - сумма транзакций за день больше дневного лимита
+//        - количество транзакций за день больше указанного лимита
+//        - если город оплаты (совершения транзакции) не разрешен
+        if (transaction.getAmount() > utils.getLimitSimpleTransactionAmount())
+            throw new LimitExceeded("Transaction limit exceeded " + transaction.getId() + ". Can'tbe saved");
+
+        int sum = 0;
+        int count = 0;
+        for (Transaction tr : getTransactionsPerDay(transaction.getDateCreated())) {
+            sum += tr.getAmount();
+            count++;
+        }
+        if (sum > utils.getLimitTransactionsPerDayAmount())
+            throw new LimitExceeded("Transaction limit per day amount exceeded " + transaction.getId() + ". Can'tbe saved");
+
+        if (count > utils.getLimitTransactionsPerDayCount())
+            throw new LimitExceeded("Transaction limit per day count exceeded " + transaction.getId() + ". Can'tbe saved");
     }
 }
