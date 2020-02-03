b0b7d068f4a79abf090a23b019448eaa0cc9e1e3
BePHuTe_CaMoKaT <eban7346@gmail.com>
2020-02-03 14:07:58 +0400

    third

--- src/lesson30/hmwrk/Controller.java
+++ src/lesson30/hmwrk/Controller.java
@@ -1,9 +1,27 @@
 package lesson30.hmwrk;
 
-public class Controller {
-    public void employeesByProject(String projectName){
+import lesson30.HW.Employee;
+import lesson30.HW.EmployeeDAO;
+import lesson30.HW.Project;
 
-    }
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Controller {
+    public static TreeSet<lesson30.HW.Employee> employeesByProject(String projectName) {
+        Iterator<lesson30.HW.Employee> employeeIterator = EmployeeDAO.getAll().iterator();
+        TreeSet<lesson30.HW.Employee> Temp = new TreeSet<>();
+
+        while (employeeIterator.hasNext()) {
+            Employee employee = employeeIterator.next();
+            for (Project project : employee.getProjectSet()) {
+                if (project.getName().equals(projectName)) {
+                    Temp.add(employee);
+                }
+            }
+        }
+        return Temp;
+    } //список сотрудников, работающих над заданным проектом
 
     private static void checkProjectForExisting(String projectName){
 
